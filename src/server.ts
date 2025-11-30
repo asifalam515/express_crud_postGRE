d02ca876b507c6b8fa34c8002fc5db6eb792812b
asifalam515 <asibulalam515@gmail.com>
2025-11-30 12:32:49 +0600

    post request applied

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
+import dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import path from "path";
 import { Pool } from "pg";
 const app = express();
 const port = 5000;
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 // parser
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 // db=>database
 const pool = new Pool({
-  connectionString: `postgresql://neondb_owner:npg_Yu9cTxP7aXZr@ep-rough-boat-a1rlirjl-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+  connectionString: `${process.env.CONNECTION_STRING}`,
 });
 const initDB = async () => {
   await pool.query(`
@@ -24,19 +28,52 @@ const initDB = async () => {
   `);
   await pool.query(`CREATE TABLE IF NOT EXISTS todos(
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id))`);
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    title VARCHAR(200) NOT NULL,
+    description TEXT,
+    completed BOOLEAN DEFAULT false,
+    due_date DATE,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()    
+    )`);
 };
 
 initDB();
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
-app.post("/", (req: Request, res: Response) => {
-  console.log(req.body);
-  res.status(200).json({
-    success: true,
-    message: "api is working",
-  });
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email, age } = req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name,email,age)VALUES($1,$2,$3)RETURNING*`,
+      [name, email, age]
+    );
+    res.status(201).json({
+      success: true,
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+// get all data
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const allUsers = await pool.query(`SELECT * FROM users`);
+    res.status(200).json({
+      success: true,
+      data: allUsers,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
