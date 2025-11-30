0b7c921cdb1c015a0bc13069796a1f7771ab179f
asifalam515 <asibulalam515@gmail.com>
2025-11-30 10:38:14 +0600

    second table created

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,33 @@
 import express, { Request, Response } from "express";
+import { Pool } from "pg";
 const app = express();
 const port = 5000;
 // parser
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded({ extended: true }));
+// db=>database
+const pool = new Pool({
+  connectionString: `postgresql://neondb_owner:npg_Yu9cTxP7aXZr@ep-rough-boat-a1rlirjl-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+});
+const initDB = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(150) UNIQUE NOT NULL,
+      age INT,
+      phone VARCHAR(15),
+      address TEXT,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    );
+  `);
+  await pool.query(`CREATE TABLE IF NOT EXISTS todos(
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id))`);
+};
 
+initDB();
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
